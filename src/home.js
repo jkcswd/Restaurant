b35fc36caf88c8ec8a063ed56b875df836b71a4e
James Spencer <jameskcs1254@gmail.com>
2021-08-19 13:06:10 +0100

    Change divCreate to include class creation

--- src/home.js
+++ src/home.js
@@ -1,15 +1,23 @@
-const divCreate = () => {
+const divCreate = (classList) => {
     //as this would be used a lot I created a function for sticking to DRY principles.
-     return document.createElement('div');
+     return document.createElement('div').classList.add(classList);
 }
 
 const homeLoad = () => {
+    //create all elements used with classes.
     const page = document.querySelector('#page');
-    const header = divCreate().classList.add('header');
-    const main = divCreate().classList.add('main');
-    const footer = divCreate().classList.add('footer');
-    const content = divCreate().classList.add('content');
-    const about = divCreate().classList.add('content-box about');
-    const opening = divCreate().classList.add('content-box opening');
-    const location = divCreate().classList.add('content-box location');
+    const header = divCreate('header');
+    const main = divCreate('main');
+    const footer = divCreate('footer');
+    const content = divCreate('content');
+    const about = divCreate('content-box about');
+    const opening = divCreate('content-box opening');
+    const location = divCreate('content-box location');
+
+    // Append text to elements
+
+    //append elements to the page
 }
+
+
+export default {homeLoad};
