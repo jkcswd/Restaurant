085e037137b262d5f32a749687bb07b94852fb95
James Spencer <jameskcs1254@gmail.com>
2021-08-20 19:44:25 +0100

    fix homeLoad with sub function and calls to sub-function

--- src/home.js
+++ src/home.js
@@ -1,18 +1,30 @@
 const homeLoad = () => {
-    //create all elements used with classes.
+    //Function to load the home page of website dynamically onto a empty DOM
+    //sub function to allow creation,appention to a DOM node, and adding a class
+    const nodeClassCreate = (classlist, targetNode) => {
+        const div = document.createElement('div');
+        div.classList.add(classlist);
 
-    const page = document.createElement('div').classList.add('page');
-    const header = document.createElement('div').classList.add('header');
-    const main = document.createElement('div').classList.add('main');
-    const footer = document.createElement('div').classList.add('footer');
-    const content = document.createElement('div').classList.add('content');
-    const about = document.createElement('div').classList.add('content-box about');
-    const opening = document.createElement('div').classList.add('content-box opening');
-    const location = document.createElement('div').classList.add('content-box location');
+        if (targetNode === 'body') {
+            document.body.appendChild(div);
+        }else {
+            const target = document.querySelector(targetNode);
+            target.appendChild(div)
+        }
+    }
+
+    //create all elements used with classes and attach to DOM in correct order.
+    const page = nodeClassCreate('page', 'body');
+    const header = nodeClassCreate('header', '.page');
+    const main = nodeClassCreate('main', '.page');
+    const footer = nodeClassCreate('footer', '.page');
+    const content = nodeClassCreate('content', '.main');
+    const about = nodeClassCreate('content-box', '.content');
+    const opening = nodeClassCreate('content-box', '.content');
+    const location = nodeClassCreate('content-box', '.content');
 
     // Append text to elements
-
-    //append elements to the page
+    
 }
 
 
