39b9bcce0aa1c01818f238d430738706f9208d49
James Spencer <jameskcs1254@gmail.com>
2021-08-21 11:54:46 +0100

    add imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './styles.css';
 import homeLoad from './home';
-import { initLoad } from './utilities'
-import contactLoad from './contact'
+import { initLoad } from './utilities';
+import contactLoad from './contact';
+import menuLoad from './menu'
 
 initLoad();
 homeLoad();
