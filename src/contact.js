ab05f98854f4f389048faa41f395c39b5a0c086c
James Spencer <jameskcs1254@gmail.com>
2021-08-21 11:24:48 +0100

    finish contact page load function

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,9 @@
 import { nodeClassCreate } from "./utilities";
 
 const contactLoad = () => {
-    
+    const contactHtml = '<p>Please contact us in the following ways:</p><p>Email:fake@fakemail.com</p><p>Phone:01234 5678</p>'
+
+    nodeClassCreate('content-box','.content',contactHtml);
 }
+
+export default contactLoad;
