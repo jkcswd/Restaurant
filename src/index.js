acc634464c79019125508f36abc34dc9519e956b
James Spencer <jameskcs1254@gmail.com>
2021-08-22 11:07:19 +0100

    create eventlisteners for tabs

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import { initLoad } from './utilities';
 import contactLoad from './contact';
 import menuLoad from './menu'
 
-initLoad();
-homeLoad();
 
 const tabListeners = () => {
-    
+    const home = document.querySelector('.home');
+    const menu = document.querySelector('.menu');
+    const contact = document.querySelector('.contact');
+
+    home.addEventListener('click', () => {
+        initLoad();
+        homeLoad();
+        tabListeners();
+    });
+    menu.addEventListener('click', () => {
+        initLoad();
+        menuLoad();
+        tabListeners();
+    });
+    contact.addEventListener('click', () => {
+        initLoad();
+        contactLoad();
+        tabListeners();
+    });
 }
+
+
+initLoad();
+homeLoad();
+tabListeners();
